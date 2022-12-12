89b4f8a49933f99cc3a844a0aca0e237221a2915
Evgeny <evolenko@inbox.ru>
2022-12-12 23:29:02 +0300

    Added Task_047

---
+++ Task_047HW_Random_double/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// ​m = 3, n = 4.
+// ​0,5 7 -2 -0,2
+// ​1 -3,3 8 -9,9
+// ​8 7,8 -7,1 9
+
+Console.Clear();
+const int m = 4;
+const int n = 4;
+const int startRand = -10;
+const int endRand = 10;
+
+double[,] matrix = new double [m, n];
+
+FillArray(matrix, startRand, endRand);
+PrintArray(matrix);
+
+double RandomForFill (int max, int min)
+{   
+    Random rand = new Random();
+    double digit = min + rand.NextDouble() * (max - min);
+    return Math.Round(digit, 1, MidpointRounding.AwayFromZero);    
+}
+
+void FillArray (double[,] mat, int st, int en)
+{
+    for (int i = 0; i < mat.GetLength(0); i++) // Переходим по строкам
+    {
+        for (int j = 0; j < mat.GetLength(1); j++) // Переходим по столбцам
+        {
+            mat[i, j] = RandomForFill(st, en);
+        }
+    }
+}
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // Переходим по строкам
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // Переходим по столбцам
+        {
+            Console.Write($"{matr[i, j]} | ");
+        }
+        Console.WriteLine();
+    }
+}
