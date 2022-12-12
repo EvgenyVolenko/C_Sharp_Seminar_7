d7a25a76bb3176316267fc7dd75d5eba577f8ef3
Evgeny <evolenko@inbox.ru>
2022-12-12 22:15:18 +0300

    Added Task_048

---
+++ Task_048CW_Count_Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
+// Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Clear();
+const int m = 3;
+const int n = 4;
+
+int[,] matrix = new int [m, n];
+
+FillArray(matrix);
+PrintArray(matrix);
+
+void FillArray (int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++) // Переходим по строкам
+    {
+        for (int j = 0; j < mat.GetLength(1); j++) // Переходим по столбцам
+        {
+            mat[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // Переходим по строкам
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // Переходим по столбцам
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
